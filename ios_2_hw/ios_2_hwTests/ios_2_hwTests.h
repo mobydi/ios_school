3b0706c4cc1ad9185a9a86f9aa9a6b657a8b9fbb
mobydi <mobydi@gmail.com>
2012-08-07 23:11:03 +0400

    homework 2 started

---
+++ ios_2_hw/ios_2_hwTests/ios_2_hwTests.h
@@ -0,0 +1,13 @@
+//
+//  ios_2_hwTests.h
+//  ios_2_hwTests
+//
+//  Created by ilya on 07.08.12.
+//  Copyright (c) 2012 ilya. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ios_2_hwTests : SenTestCase
+
+@end
---
+++ ios_2_hw/ios_2_hwTests/ios_2_hwTests.m
@@ -0,0 +1,32 @@
+//
+//  ios_2_hwTests.m
+//  ios_2_hwTests
+//
+//  Created by ilya on 07.08.12.
+//  Copyright (c) 2012 ilya. All rights reserved.
+//
+
+#import "ios_2_hwTests.h"
+
+@implementation ios_2_hwTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ios_2_hwTests");
+}
+
+@end

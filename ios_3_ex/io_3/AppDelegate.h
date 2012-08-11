fdd269b742ba42f6fe065afee79c07891df105ab
mobydi <mobydi@gmail.com>
2012-08-11 10:57:43 +0400

    hw 3 extra

---
+++ ios_3_ex/io_3/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  io_3
+//
+//  Created by ilya on 08.08.12.
+//  Copyright (c) 2012 ilya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ios_3_ex/io_3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  io_3
+//
+//  Created by ilya on 08.08.12.
+//  Copyright (c) 2012 ilya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
